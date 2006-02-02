c594ab54e2f28e56689ece727eda22b8ca0a0ec6
Jake Gordon <jakeg@15674.no-reply.drupal.org>
2006-02-02 11:26:23 +0000

    Thanks to Simon Rawson for much time spent on this: - Makes it possible to have non-fixed widths for horizontal (dropdown) menus - Should fix conflict with other modules and Javascript onLoad - should fix caching problem with javascript called twice (thanks moshe, http://drupal.org/node/44664 ) - maybe fixes problems people have had with IE?

--- nice_menus.js
+++ nice_menus.js
@@ -1,38 +1,41 @@
-function IEHoverPseudo() {
+//We only add the javascript to IE.
+if (document.all) {
 
-  var ulNodes = getElementsByClass("nice-menu");
+  function IEHoverPseudo() {
 
-  var j = 0;
-  var liNodes = null;
+    var ulNodes = getElementsByClass("nice-menu");
+    var j = 0;
+    var liNodes = null;
 
-  for(var i = 0; i < ulNodes.length; i++) { 
-    liNodes = ulNodes[i].getElementsByTagName("li");
-    for(j = 0; j < liNodes.length; j++) {
-      if(liNodes[j].className == "menuparent") {
-        liNodes[j].onmouseover=function() { this.className += " over"; }
-        liNodes[j].onmouseout=function() { this.className = "menuparent"; }
+    for(var i = 0; i < ulNodes.length; i++) { 
+      liNodes = ulNodes[i].getElementsByTagName("li");
+      for(j = 0; j < liNodes.length; j++) {
+        if(liNodes[j].className == "menuparent") {
+          liNodes[j].onmouseover=function() { this.className += " over"; }
+          liNodes[j].onmouseout=function() { this.className = "menuparent"; }
+        }
       }
     }
   }
-}
 
-
-function getElementsByClass(searchClass,node,tag) {
+  function getElementsByClass(searchClass,node,tag) {
 	var classElements = new Array();
-	if ( node == null )
-		node = document;
-	if ( tag == null )
+	  if ( node == null )
+        node = document;
+	  if ( tag == null )
 		tag = '*';
-	var els = node.getElementsByTagName(tag);
-	var elsLen = els.length;
-	var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
-	for (i = 0, j = 0; i < elsLen; i++) {
-		if ( pattern.test(els[i].className) ) {
+	  var els = node.getElementsByTagName(tag);
+	  var elsLen = els.length;
+	  var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+	  for (i = 0, j = 0; i < elsLen; i++) {
+		  if ( pattern.test(els[i].className) ) {
 			classElements[j] = els[i];
 			j++;
-		}
-	}
+		  }
+	  }
 	return classElements;
-}
+  }
 
-window.onload = IEHoverPseudo;
+  //This is the Drupal method of adding a function to the BODY onload event.  (See misc/drupal.js)
+  addLoadEvent(IEHoverPseudo);
+}
