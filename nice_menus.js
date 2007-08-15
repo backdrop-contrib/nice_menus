f91d2195666892680f217be0e47becedd86c21ab
Chad Phillips <chad@apartmentlines.com>
2007-08-15 14:59:40 +0000

    Holy cow, this is what happens when you try to patch and commit across two machines.  These files should actually work now.

--- nice_menus.js
+++ nice_menus.js
@@ -1,43 +1,16 @@
 // $Id$
 
-// We only do the javascript in IE.
-// TODO: because we now only include the js file for IE, is this 'if' redundant?
-
-// Thanks to 'cooperaj' for initial jQuery work
-$(document).ready(function(){
-  $("ul.nice-menu li").hoverClass("over");
-});
-
-$.fn.hoverClass = function(c) {
-  return this.each(function(){
-    $(this).hover( 
-      function() { 
-        $(this).addClass(c).find("> div/ul").slideDown("slow").addShim(); 
-      },
-      function() { 
-        $(this).find("> div/ul").removeShim().end().removeClass(c);
-      }
-    );
-  });
-};
-
-$.fn.addShim = function() {
-  return this.each(function(){
-    /* on for IE with document.all */
-    if(document.all && $("select").size() > 0) {
-      var ifShim = document.createElement('iframe');
-      ifShim.style.width=$(this).width()+1+"px";
-      ifShim.style.height=$(this).find("> li").size()*23+4+"px";
-      ifShim.style.filter="progid:DXImageTransform.Microsoft.Alpha(style=0,opacity=0)";
-      ifShim.style.zIndex="0";
-      $(this).prepend(ifShim);
-      $(this).css("zIndex","99");
+// We only do the javascript in IE thanks to drupal_set_html_head in .module.
+if (document.all) {
+  function IEHoverPseudo() {
+      $("ul.nice-menu li.menuparent").hover(function(){
+          $(this).addClass("over").find("> ul").slideDown("fast");
+        },function(){
+          $(this).removeClass("over").find("> ul").slideUp("fast");
+        }
+      );
     }
-  });
-};
 
-$.fn.removeShim = function() {
-  return this.each(function(){
-    if (document.all) $("iframe", this).remove();
-  });
-};
+    // This is the jquery method of adding a function to the BODY onload event.  (See jquery.com)
+    $(document).ready(function(){ IEHoverPseudo() });
+}
