3c914c79eada759542bc74f79ca96fdcd00bfa5d
Matt Kleve (vordude) <vordude@150473.no-reply.drupal.org>
2012-05-30 10:43:54 -0600

    re: [#1363316] Add Nice Menus to Drupal.behaviors function deviantintegral is the fricken man

--- nice_menus.js
+++ nice_menus.js
@@ -1,24 +1,38 @@
+/**
+ * @file
+ * Behaviours for the Nice Menus module. This uses Superfish 1.4.8.
+ *
+ * @link http://users.tpg.com.au/j_birch/plugins/superfish
+ */
 
-// This uses Superfish 1.4.8
-// (http://users.tpg.com.au/j_birch/plugins/superfish)
-
-// Add Superfish to all Nice menus with some basic options.
 (function ($) {
-  $(document).ready(function() {
-    $('ul.nice-menu').superfish({
-      // Apply a generic hover class.
-      hoverClass: 'over',
-      // Disable generation of arrow mark-up.
-      autoArrows: false,
-      // Disable drop shadows.
-      dropShadows: false,
-      // Mouse delay.
-      delay: Drupal.settings.nice_menus_options.delay,
-      // Animation speed.
-      speed: Drupal.settings.nice_menus_options.speed
-    // Add in Brandon Aaron’s bgIframe plugin for IE select issues.
-    // http://plugins.jquery.com/node/46/release
-    }).find('ul').bgIframe({opacity:false});
-    $('ul.nice-menu ul').css('display', 'none');
-  });
+
+  /**
+   * Add Superfish to all Nice Menus with some basic options.
+   */
+  Drupal.behaviors.niceMenus = {
+    attach: function (context, settings) {
+      $('ul.nice-menu:not(.nice-menus-processed)').addClass('nice-menus-processed').each(function () {
+        $(this).superfish({
+          // Apply a generic hover class.
+          hoverClass: 'over',
+          // Disable generation of arrow mark-up.
+          autoArrows: false,
+          // Disable drop shadows.
+          dropShadows: false,
+          // Mouse delay.
+          delay: Drupal.settings.nice_menus_options.delay,
+          // Animation speed.
+          speed: Drupal.settings.nice_menus_options.speed
+        });
+
+        // Add in Brandon Aaron’s bgIframe plugin for IE select issues.
+        // http://plugins.jquery.com/node/46/release
+        $(this).find('ul').bgIframe({opacity:false});
+
+        $('ul.nice-menu ul').css('display', 'none');
+      });
+    }
+  };
+
 })(jQuery);
